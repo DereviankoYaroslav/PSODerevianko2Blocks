44d402690e399d5b1e88af1c865361a1148c4c7f
olenaderevianko <dereviankoolenav@gmail.com>
2021-09-23 16:36:46 +0300

    PSO +2blocks 110NL

---
+++ cmake-build-debug/CMakeFiles/PSODerevianko2Blocks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSODerevianko2Blocks.dir/main.c.obj"
+  "PSODerevianko2Blocks.exe"
+  "PSODerevianko2Blocks.exe.manifest"
+  "PSODerevianko2Blocks.pdb"
+  "libPSODerevianko2Blocks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PSODerevianko2Blocks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
